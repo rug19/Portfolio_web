9c7183dc432971223f895b8ccb5889237564a5d8
Ruan <rugcosta1234@gmail.com>
2024-11-28 22:51:19 -0300

    Feito alguns reparos no layout na tela home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,36 @@
 export default function Home() {
   return (
     <>
-      <div>
-        <p>Home</p>
+      {/* Main container */}
+      <div className=" h-screen flex justify-center items-center">
+        <div className="flex flex-col-reverse justify-around flex-justify md:flex-row items-center gap-20">
+          <div className=" md:flexitems-center">
+            <h1 className=" text-6xl mb-6 font-sans text-white">
+              Nice to meet you!
+              <br />
+              <span className="text-green">I'm Ruan Gomes</span>
+            </h1>
+            <p className=" max-w-[700px] mb-9 font-sans text-white ">
+              Im a full-stack developer dedicated to crafting comprehensive
+              solutions for digital challenges.From user interface to backend,
+              Im constantly seeking ways to make technology more accessible and
+              efficient.
+            </p>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+              CONTACT-ME
+            </button>
+          </div>
+
+          {/* Img container */}
+          <div>
+            <img
+              src="../../public/Design sem nome (5).png"
+              alt="Descrição da imagem"
+              width="300"
+              height="300"
+            ></img>
+          </div>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1c1c22",
+        green: "#00e187",
+      },
+      fontFamily: {
+        sans: ["JetBrains Mono", "monospace"], // Define como padrão
+      },
+    },
   },
   plugins: [],
-}
-
+};
