eb875a157822f76e8e211022ecbb46aa1905921f
unknown <rugcosta1234@gmail.com>
2024-11-29 13:32:53 -0300

    Finalizando o layout da home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedinIn,
+  faInstagram,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import IconLink from "../components/IconButton";
+
 export default function Home() {
   return (
     <>
@@ -11,24 +21,30 @@ export default function Home() {
               <span className="text-green">I'm Ruan Gomes</span>
             </h1>
             <p className=" max-w-[700px] mb-9 font-sans text-white ">
-              Im a full-stack developer dedicated to crafting comprehensive
+              I'm a full-stack developer dedicated to crafting comprehensive
               solutions for digital challenges.From user interface to backend,
-              Im constantly seeking ways to make technology more accessible and
+              I'm constantly seeking ways to make technology more accessible and
               efficient.
             </p>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-              CONTACT-ME
-            </button>
+            <div className="flex gap-5 items-center ">
+              <button className="  border-2 border-green  text-green tracking-widest  font-semibold font-sans  text-xs px-3 py-2 rounded-full hover:bg-green hover:text-primary">
+                DOWNLOAD CV{" "}
+                <span>
+                  <FontAwesomeIcon className="text-inherit" icon={faDownload} />
+                </span>
+              </button>
+              <div className="flex gap-3">
+                <IconLink icon={faGithub} />
+                <IconLink icon={faLinkedinIn} />
+                <IconLink icon={faInstagram} />
+                <IconLink icon={faWhatsapp} />
+              </div>
+            </div>
           </div>
 
           {/* Img container */}
           <div>
-            <img
-              src="../../public/Design sem nome (5).png"
-              alt="Descrição da imagem"
-              width="300"
-              height="300"
-            ></img>
+            <img src="" alt="Photo" width="300" height="300"></img>
           </div>
         </div>
       </div>
