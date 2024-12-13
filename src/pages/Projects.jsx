b39e79b727c14ffab24f60f294176db83962233b
Ruan <rugcosta1234@gmail.com>
2024-12-12 21:00:30 -0300

    Implementando a loogica do carrossel na page project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,25 +2,63 @@ import { FaGithub } from "react-icons/fa";
 import Particle from "../components/Particle";
 import { BsArrowUpRight } from "react-icons/bs";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { useState } from "react";
 
 export default function Projects() {
+  const linkStyle =
+    "flex items-center justify-center w-11 h-11 border-2 border-green rounded-full text-green hover:bg-green hover:text-primary cursor-pointer";
+  const imgStyle = "h-[350px] w-[500px]  mb-3";
+  const buttonStyle =
+    "flex items-center justify-center w-8 h-8 border-2 border-green  text-black bg-green hover:text-primary cursor-pointer";
+  const containerStyle =
+    "h-screen sm:h-auto lg:h-auto rounded-2xl  flex flex-col lg:flex-row gap-10 lg:gap-20 bg-black bg-opacity-50 p-5 lg:p-10 w-1/1 ";
+
+
   const items = [
     {
       id: 1,
-      name: "",
-      description: "",
-      tecnolohies: "",
-      liveUrl: "",
-      gitUrl: "",
-      image: "/public/desktop-preview.jpg",
+      number: "01",
+      title: "Frontend Project",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum",
+      tecnolohies: " HTML, Css 3, JavaScript",
+      liveUrl:
+        "https://rug19.github.io/huddle-landing-page-with-curved-sections-master/",
+      gitUrl:
+        "https://github.com/rug19/huddle-landing-page-with-curved-sections-master.git",
+      image: "/desktop-preview.jpg",
       alt: "Land page",
     },
+
+    {
+      id: 2,
+      number: "02",
+      title: "Frontend Project",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum",
+      tecnolohies: " React, JavaScript, Css",
+      liveUrl:
+        "https://rug19.github.io/huddle-landing-page-with-curved-sections-master/",
+      gitUrl:
+        "https://github.com/rug19/huddle-landing-page-with-curved-sections-master.git",
+      image: "/desktop-design.jpg",
+      alt: "Dashboard",
+    },
+   
+
+    
   ];
-  const linkStyle =
-    "flex items-center justify-center w-11 h-11 border-2 border-green rounded-full text-green hover:bg-green hover:text-primary cursor-pointer";
-  const imgStyle = "h-[350px]  mb-3";
-  const buttonStyle =
-    "flex items-center justify-center w-8 h-8 border-2 border-green  text-black bg-green hover:text-primary cursor-pointer";
+
+  const [indiceAtual, setIndiceAtual] = useState(0);
+
+  const handleBefore = () => {
+    setIndiceAtual((prev) => (prev === 0 ? items.length - 1 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setIndiceAtual((prev) => (prev === items.length - 1 ? 0 : prev + 1)); 
+  };
+
   return (
     <>
       <div>
@@ -37,44 +75,61 @@ export default function Projects() {
           <Particle />
         </div>
         <section className="h-screen flex justify-center items-center ">
-          <div className="h-screen sm:h-auto lg:h-auto rounded-2xl  flex flex-col lg:flex-row gap-10 lg:gap-20 bg-black bg-opacity-50 p-5 lg:p-10 w-1/1 ">
+          <div key={items[indiceAtual].id} className={containerStyle}>
             <div className="flex flex-col gap-5">
-              <h1></h1>
+              <h1 className="text-white text-6xl">
+                {items[indiceAtual].number}
+              </h1>
               <h2 className=" font-sans text-4xl font-bold text-white">
-                Frontend Project
+                {items[indiceAtual].title}
               </h2>
               <p className="text-white md:max-w-[600px] lg:max-w-[400px] font-sans  text-center lg:text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-                nemo repudiandae nulla placeat reprehenderit aliquid.
+                {items[indiceAtual].description}
               </p>
               <p className="text-green font-sans text-2xl  border-b-[1px] pb-5">
-                HTML, Css 3, JavaScript
+                {items[indiceAtual].tecnolohies}
               </p>
               <div className="flex gap-3">
-                <a className={linkStyle} href="">
+                <a
+                  className={linkStyle}
+                  target="_blank"
+                  href={items[indiceAtual].liveUrl}
+                >
                   {" "}
                   <BsArrowUpRight className="size-6 " />
                 </a>
-                <a className={linkStyle} href="">
+                <a
+                  className={linkStyle}
+                  target="_blank"
+                  href={items[indiceAtual].gitUrl}
+                >
                   <FaGithub className="size-6  " />
                 </a>
               </div>
             </div>
             <div>
-              {items.map((item) => (
-                <div key={item.id}>
-                  <img className={imgStyle} src={item.image} alt={item.alt} />
-                  <div className="flex justify-end gap-2">
-                    <button className={buttonStyle}>
-                      <IoIosArrowBack className="size-5" />
-                    </button>
-                    <button className={buttonStyle}>
-                      {" "}
-                      <IoIosArrowForward className="size-5" />
-                    </button>
-                  </div>
-                </div>
-              ))}
+              <img
+                className={imgStyle}
+                src={items[indiceAtual].image}
+                alt={items[indiceAtual].alt}
+              />
+              <div className="flex justify-end gap-2">
+                <button
+                  onClick={handleBefore}
+                  className={buttonStyle}
+                  disabled={indiceAtual === 0}
+                >
+                  <IoIosArrowBack className="size-5" />
+                </button>
+                <button
+                  onClick={handleNext}
+                  className={buttonStyle}
+                  disabled={indiceAtual === items.length - 1}
+                >
+                  {" "}
+                  <IoIosArrowForward className="size-5" />
+                </button>
+              </div>
             </div>
           </div>
         </section>
