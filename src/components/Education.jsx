6c5f39ef54dd7178dcf8335123e4677b8bb72b6a
Ruan <rugcosta1234@gmail.com>
2024-12-22 20:22:59 -0300

    Ajustando o layout

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -49,7 +49,7 @@ export default function Education() {
         <div className=" flex flex-col  max-h-96 sm:max-h-72 max-h-65  w-full overflow-auto scrollbar-thin scrollbar-thumb-green scrollbar-track-transparent pt-2 lg:pt-0 sm:grid sm:grid-cols-2 gap-5 lg:gap-4 mt-8 mb-9 sm:mb-0 p-0">
           {items.map((item) => (
             <div
-              className="flex-1 flex flex-col justify-center items-center  sm:items-start  text-center sm:text-start h-32  max-w-[100%] p-4 border border-green font-bold text-white rounded font-sans"
+              className="flex-1 flex flex-col justify-center items-center  sm:items-start  text-center sm:text-start h-32  max-w-[100%] p-4 bg-gray-800 bg-opacity-50 font-bold text-white rounded font-sans"
               key={item.id}
             >
               <p className="text-green">{item.year}</p>
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,6 +1,8 @@
 import Particle from "../components/Particle";
 
 export default function Contacts() {
+  const inputStyle =
+    "bg-gray-800 bg-opacity-50 text-white p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-green focus:ring-opacity-50 font-sans h-10 w-full ";
   return (
     <section>
       <div
@@ -19,15 +21,37 @@ export default function Contacts() {
         <div className="rounded-2xl flex justify-center items-center flex-col-reverse lg:flex-row gap-10 lg:gap-20 bg-black bg-opacity-50 p-5 lg:p-10 w-full max-w-7xl">
           <div>
             {" "}
-            <h1 className="text-white text-4xl">Let's work together</h1>
+            <h1 className="text-white text-4xl font-sans">
+            Let&apos;s work together
+            </h1>
             <p className="text-white md:max-w-[600px] lg:max-w-[400px] font-sans text-justify">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam
               labore corporis vitae in tempora voluptates quas laudantium
               impedi.
             </p>
-            <form action="">
-              <label></label>
-              <input type="text" />
+            <form action="" method="post">
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                <input
+                  className={inputStyle}
+                  type="firsnamet"
+                  placeholder="Firstname"
+                />
+                <input
+                  className={inputStyle}
+                  type="lastname"
+                  placeholder="Lastname"
+                />
+                <input
+                  className={inputStyle}
+                  type="email"
+                  placeholder="Email address"
+                />
+                <input
+                  className={inputStyle}
+                  type="phone"
+                  placeholder="Phone number"
+                />
+              </div>
             </form>
           </div>
         </div>
