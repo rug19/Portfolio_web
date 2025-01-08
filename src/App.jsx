f88f73207a0765e6b89cb532c89fa19a916a0372
unknown <rugcosta1234@gmail.com>
2025-01-08 13:37:20 -0300

    Ajustando as configurações do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/Portfolio_web">
+      <BrowserRouter>
         <Header />
         <AppRoutes />
       </BrowserRouter>
